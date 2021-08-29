0a4318ebbb28e070237b31533367a44f71e21b79
unknown <turetcan@gmail.com>
2021-08-29 20:41:47 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+ <div class="container">
+   <app-tags v-model="tags" color="success"></app-tags>
+   <app-tags v-model="tags2" color="danger"></app-tags>
+   <app-tags v-model="tags2" color="info"></app-tags>
+   <app-tags v-model="tags2" color="primary"></app-tags>
+   <app-tags v-model="tags2" color="warning"></app-tags>
+   <app-tags v-model="tags2" color="secondary"></app-tags>
+
+ </div>
+</template>
+
+<script>
+import Tags from "./components/Tags"
+export default {
+  components : {
+    appTags : Tags
+  },
+  data(){
+    return {
+      tags : "test1,test2,test3",
+      tags2 : "deneme1,deneme2,deneme3"
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+}
+
+
+</style>
